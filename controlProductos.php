923ae1efa6396f5a2d4572c7499336e73aeb62c3
Jose_Martinez <martinez022jose@gmail.com>
2020-04-30 11:46:17 -0300

    Se Agrega 'Control de producto'

---
+++ controlProductos.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Control Productos</title>
+	<link rel="stylesheet" type="text/css" href="estiloProductos.css">
+</head>
+<body>
+	<header class="headerControl">
+		<div class="title">
+			<h1>Control De Productos</h1>
+		</div>
+		<div class="enlaces">
+			<a href="controlProductos.php">Inicio</a>
+		    <a href="listaDeProductos.php">Listado de productos</a>
+		    <a href="registroProductos.php">Registro de producto</a>
+		    <a href="eliminarProductoSegun.php">Eliminar segun</a>
+				
+		</div>
+	</header>
+	
+	<section>
+		<div class="bienvenida">
+			<h2>Bienvenido al control de productos</h2>
+		</div>
+	</section>
+
+</body>
+</html>
