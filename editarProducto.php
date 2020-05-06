b00d18a167b004ad365fba3aed522d130ceacdcf
Jose_Martinez <martinez022jose@gmail.com>
2020-05-06 15:57:50 -0300

    Se agrega funcionalidad 'editarRegistro'

--- editarProducto.php
+++ editarProducto.php
@@ -1,11 +1,23 @@
 <?php
+
+$errores = Array();
+
 function obtenerRegistro($idProducto,$conexion){
 	$queryBusquedaRegistro = "SELECT *FROM productos WHERE idProducto = '$idProducto'";
 	$consultaRegistro = mysqli_query($conexion,$queryBusquedaRegistro);
     $registro = mysqli_fetch_array($consultaRegistro);
     return $registro;
 }
+
+function actualizarDatos($idProducto,$nombreProd,$descProd,$precioProd,$url,$conexion){
+	$queryUpdate = "UPDATE productos SET 
+	                nombre='$nombreProd',descripcion='$descProd',precio='$precioProd', idProducto = '$idProducto' WHERE url='$url'";
+     $actualizado = mysqli_query($conexion,$queryUpdate);
+     return $actualizado;
+}
+
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,57 +37,59 @@ function obtenerRegistro($idProducto,$conexion){
 				
 		</div>
 	</header>
+	<?php
+	include ("conexion.php");
+    $idProducto = $_GET['idProducto'];
+    $producto = obtenerRegistro($idProducto,$conexion);
+    
 
+    if(!empty($_POST['submit'])){
+
+	$idProducto =$_POST['nuevoIdProd'];
+    $nombre = $_POST['nuevoNombreProd'];
+    $descripcion = $_POST['nuevaDescProd'];
+    $precio = $_POST['nuevoPrecioProd'];
+    $url = $producto['url'];
+
+   if(actualizarDatos($idProducto,$nombre,$descripcion,$precio,$url,$conexion))
+   {
+   	header("location:listaDeProductos.php");
+   }
+   else{
+   	 array_push($errores,"<p class='error'> No se ha actualizado producto</p>");
+   }
+}
+	?>
 	<section>
-		<div class="form">
+		<form method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" class="form" enctype="multipart/form-data">
+
 			<div class="informacion">
 				<h2>¿Estas seguro de eliminar este producto?</h2>
-			
-			<?php
-				include ("conexion.php");
-				$idProducto = $_GET['idProducto'];
-				$producto = obtenerRegistro($idProducto,$conexion);
-			?>
-
-			<div class="registro">
-					<div class="elemento">
-						<div class="label">Codigo Producto: </div>
-						<input type="text" class="contenido" value="<?php echo $producto['idProducto'];?>">
-						
-					</div>
-					<div class="elemento">
-						<div class="label">Nombre Producto: </div>
-						<input type="text" class="contenido" value="<?php echo $producto['nombre'];?>">
-						
-					</div>
-					<div class="elemento">
-						<div class="label">Descripcion Producto:</div>
-						<textarea class="textarea"><?php echo $producto['descripcion'];?></textarea>
-						
-					</div>
-				    <div class="elemento">
-						<div class="label">Precio Producto: </div>
-						<input type="text" class="contenido" value="<?php echo $producto['precio'];?>">
-						
-					</div>
-
-				    <div>Imagen Producto:</div>
-				    <div class="cajaImagen">
-				    	<img src="<?php echo $producto['url'];?>">
-				    </div>
-				  
-					
-				</div>
-
-				<form class="confirmacionOperacion" method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" >
-
-					<input id="buttonEliminar" type="submit" name="submit" class="referencia" value="Editar">
-					<a  id="buttonCancelar" class="referencia" href="listaDeProductos.php">Cancelar</a>
-					
-				</form>
-			  </div>
 			</div>
-	</section>
+	
+			<label>Codigo Producto: </label>
+			<input class="contenido" name="nuevoIdProd" value="<?php echo $producto['idProducto'];?>">
+						
+			<label>Nombre Producto: </label>
+			<input type="text" name="nuevoNombreProd" class="contenido" value="<?php echo $producto['nombre'];?>">
+						
+		
+			<label>Descripcion Producto:</label>
+			<textarea name="nuevaDescProd"class="textarea"><?php echo $producto['descripcion'];?></textarea>
+						
+		    <label>Precio Producto: </label>
+			<input name="nuevoPrecioProd" type="text" class="contenido" value="<?php echo $producto['precio'];?>">
+					
+			<label>Imagen Producto:</label>
+			<div class="cajaImagen">
+				<img src="<?php echo $producto['url'];?>">
+			</div>
+
+			<input id="buttonEliminar" type="submit" name="submit" class="referencia" value="Editar">
+			<a  id="buttonCancelar" class="referencia" href="listaDeProductos.php">Cancelar</a>
+				 
+			</form>
+		</section>
 
 </body>
 </html>
