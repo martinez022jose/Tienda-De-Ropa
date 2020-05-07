60f4f0678630079010ef4dea24d18e3cd6e3a89c
Jose_Martinez <martinez022jose@gmail.com>
2020-05-06 22:58:51 -0300

    Se agrega interfaz 'user'

---
+++ user.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User</title>
+	<link rel="stylesheet" type="text/css" href="estiloProductos.css">
+</head>
+<body>
+	<section>
+		<div class="cajaFormulario">
+			
+		
+		<form method="POST" action="<?php htmlspecialchars($_SERVER['PHP_SELF']);?>" class="formUsuario" enctype="multipart/form-data">
+
+			
+		    <h2>Sign in</h2>
+		    
+		    <label>Usuario: </label>
+			<input type="text" name="nombreDeUsuario" placeholder="Ingrese Usuario">
+						
+			<label>Contraseña: </label>
+			<input type="text" name="contraseñaDeUsuario" placeholder="Ingrese Contraseña">
+
+			<input id="buttonRegistro" type="submit" name="submit" values="Ingresar">
+		    
+			</form>
+		</div>
+		</section>
+
+</body>
+</html>
