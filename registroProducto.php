25f2516fe6a0aa104366bb44263f132b5a746ba1
Jose_Martinez <martinez022jose@gmail.com>
2020-05-07 19:02:21 -0300

    Se agrega header control homogeneo

--- registroProducto.php
+++ registroProducto.php
@@ -4,22 +4,35 @@
 <head>
 	<title>registroProducto</title>
 	<link rel="stylesheet" type="text/css" href="estiloProductos.css">
+	<script src="https://kit.fontawesome.com/59dceabbec.js" crossorigin="anonymous"></script>
 </head>
 <body>
 	<header class="headerControl">
-		<div class="title">
-			<h1>Control De Productos</h1>
-		</div>
-		<div class="enlaces">
-			<a href="controlProductos.php">Inicio</a>
-		    <a href="listaDeProductos.php">Listado de productos</a>
-		    <a href="registroProducto.php">Registro de producto</a>
-		    <a href="eliminarProductoSegun.php">Eliminar segun</a>
+		<div class="portada">
+		    <div class="cajaLogo">
+		    	<div class="center">
+		    		<h2 class="logo">LOGO</h2>
+			        <h1>Control De Productos</h1>
+		    	</div>
+			   
+			    <div class="rigth">
+			    	 <div id="iconoUsuario" class="far fa-user"></div>
+			         <a id="cerrarSesion" href="salida.php" class="fas fa-power-off"></a>
+			    </div>
+			   
+		    </div>
+
+		    <div class="enlaces">
+			    <a href="controlProductos.php">Inicio</a>
+		        <a href="listaDeProductos.php">Listado de productos</a>
+		        <a href="registroProducto.php">Registro de producto</a>
+		        <a href="eliminarProductoSegun.php">Eliminar segun</a>
 				
-		</div>
+		    </div>
+	    </div>
 	</header>
 
-	<div class="separador"></div>
+	<div class="separadorControl"></div>
 
 
 	<section>
