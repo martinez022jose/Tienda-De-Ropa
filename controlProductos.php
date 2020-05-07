2a94fa671f9b9efce1cedec078519c058bb3ad2a
Jose_Martinez <martinez022jose@gmail.com>
2020-05-07 18:54:15 -0300

    Mejora estilo header control

--- controlProductos.php
+++ controlProductos.php
@@ -14,25 +14,38 @@ if(empty($_SESSION['activo'])){
 </head>
 <body>
 	<header class="headerControl">
-		<div class="title">
-			<h2>LOGO</h2>
-			<h1>Control De Productos</h1>
-			<div id="iconoUsuario" class="far fa-user"></div>
-			<a href="salida.php" class="fas fa-power-off"></a>
+		<div class="portada">
+		    <div class="cajaLogo">
+		    	<div class="center">
+		    		<h2 class="logo">LOGO</h2>
+			        <h1>Control De Productos</h1>
+		    	</div>
+			   
+			    <div class="rigth">
+			    	 <div id="iconoUsuario" class="far fa-user"></div>
+			         <a id="cerrarSesion" href="salida.php" class="fas fa-power-off"></a>
+			    </div>
+			   
+		    </div>
 
-		</div>
-		<div class="enlaces">
-			<a href="controlProductos.php">Inicio</a>
-		    <a href="listaDeProductos.php">Listado de productos</a>
-		    <a href="registroProducto.php">Registro de producto</a>
-		    <a href="eliminarProductoSegun.php">Eliminar segun</a>
+		    <div class="enlaces">
+			    <a href="controlProductos.php">Inicio</a>
+		        <a href="listaDeProductos.php">Listado de productos</a>
+		        <a href="registroProducto.php">Registro de producto</a>
+		        <a href="eliminarProductoSegun.php">Eliminar segun</a>
 				
-		</div>
+		    </div>
+	    </div>
 	</header>
+
+	<div class="separador"></div>
 	
 	<section>
-		<div class="bienvenida">
-			<h2>Bienvenido al control de productos</h2>
+		<div class="cajaBienvenida">
+			<div class="bienvenida">
+				<h2>Bienvenido al control de productos</h2>
+			</div>
+		
 		</div>
 	</section>
 
